aab124baabbdea76fa4eabb0c9180d6a3611799a
Rory <2316980y@student.gla.ac.uk>
2020-11-08 14:57:26 +0000

    Add slide normalization to pipeline

--- src/tile.py
+++ src/tile.py
@@ -5,6 +5,8 @@ import numpy as np
 import openslide
 from openslide import open_slide
 from openslide.deepzoom import DeepZoomGenerator
+import staintools
+from PIL import Image
 
 
 def get_avg_brightness(tile):
@@ -13,7 +15,34 @@ def get_avg_brightness(tile):
     return np.average(bw)
 
 
-def tile(slide_loc, output_dir, background=0.1, size=299):
+def normalize_tile(tile):
+    """
+        Normalize the staining of H&E histology slides.
+
+        This function normalizes the staining of H&E histology slides.
+        References:
+            - M. Macenko et al., "A method for normalizing histology slides for
+              quantitative analysis," 2009 IEEE International Symposium on Biomedical Imaging:
+              From Nano to Macro, Boston, MA, 2009, pp. 1107-1110, doi: 10.1109/ISBI.2009.5193250.
+            
+            - https://ieeexplore.ieee.org/document/5193250
+        Args:
+            - tile: A PIL Image object of the slide tile
+        
+        Returns:
+            - tile: A PIL Image object of the normalized slide tile
+    """
+    tile_array = np.array(tile)
+    tile_array = staintools.LuminosityStandardizer.standardize(tile_array)
+
+    normalizer = staintools.StainNormalizer(method='macenko')
+    normalizer.fit(tile_array)
+    normalized_tile = normalizer.transform(tile_array)
+
+    return Image.fromarray(normalized_tile)
+
+
+def tile(slide_loc, output_dir, background=0.1, size=255):
     slide = open_slide(slide_loc)
     dz = DeepZoomGenerator(slide, size, 0)
 
@@ -40,15 +69,15 @@ def tile(slide_loc, output_dir, background=0.1, size=299):
                 avg_brightness = get_avg_brightness(tile)
 
                 if avg_brightness < background:
+                    tile = normalize_tile(tile)
                     tile.save(tile_name + ".jpeg")
-                
 
 
 if __name__ == "__main__":
     parser = OptionParser(usage='Usage: %prog <slide> <output_folder> [options]')
     parser.add_option('-o', '--output', metavar='NAME', dest='output_dir', help='base name of output file')
     parser.add_option('-b', '--background', metavar='PIXELS', dest='background', type='float', default=0.1, help='Max background threshold [0.1]; percentage of background allowed')
-    parser.add_option('-s', '--size', metavar='PIXELS', dest='tile_size', type='int', default=299, help='tile size [299]')
+    parser.add_option('-s', '--size', metavar='PIXELS', dest='tile_size', type='int', default=255, help='tile size [299]')
     
     (opts, args) = parser.parse_args()
 
