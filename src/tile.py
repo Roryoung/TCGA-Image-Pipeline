342b3e845920a921f1ba5f953fc612613aeeaaec
Rory <2316980y@student.gla.ac.uk>
2020-11-12 15:30:23 +0000

    Add comments and documentation

--- src/tile.py
+++ src/tile.py
@@ -7,34 +7,47 @@ from openslide import open_slide
 from openslide.deepzoom import DeepZoomGenerator
 import staintools
 from PIL import Image
+import cv2
 
 
-def get_avg_brightness(tile):
+def get_background_percentage(tile):
+    """
+        This function calculates the amount of white background in a given tile.
+
+        Args:
+            - tile: A PIL Image object of the slide tile
+
+        Returns:
+            - Float: The precentage of background in the tile
+    """
+
+    #convert tile to strict black and white (not greyscale)
     grey = tile.convert("L")
-    bw = grey.point(lambda x: 0 if x< 225 else 255, "F")
+    bw = grey.point(lambda x: 0 if x>= 225 else 255)
+    
+    #floodfill black and white image
+    des = np.array(bw)
+    contour, hier = cv2.findContours(des,cv2.RETR_CCOMP,cv2.CHAIN_APPROX_SIMPLE)
 
-    bw_arr = np.array(bw)
-    black_cells = np.count_nonzero(bw_arr == 0)
-    white_cells = np.count_nonzero(bw_arr == 255)
+    for cnt in contour:
+        cv2.drawContours(des,[cnt],0,255,-1)
+
+    #calculate background %
+    bw_arr = np.array(des)
+    white_cells = np.count_nonzero(bw_arr == 0)
+    black_cells = np.count_nonzero(bw_arr == 255)
     return white_cells/(white_cells+black_cells)
 
 
 def normalize_tile(tile):
     """
-        Normalize the staining of H&E histology slides.
+        This function normalizes the staining of H&E histology slides using the macenko method.
 
-        This function normalizes the staining of H&E histology slides.
-        References:
-            - M. Macenko et al., "A method for normalizing histology slides for
-              quantitative analysis," 2009 IEEE International Symposium on Biomedical Imaging:
-              From Nano to Macro, Boston, MA, 2009, pp. 1107-1110, doi: 10.1109/ISBI.2009.5193250.
-            
-            - https://ieeexplore.ieee.org/document/5193250
         Args:
             - tile: A PIL Image object of the slide tile
         
         Returns:
-            - tile: A PIL Image object of the normalized slide tile
+            - A PIL Image object of the normalized slide tile
     """
     tile_array = np.array(tile)
     tile_array = staintools.LuminosityStandardizer.standardize(tile_array)
@@ -47,6 +60,18 @@ def normalize_tile(tile):
 
 
 def tile(slide_loc, output_dir, background=0.2, size=255):
+    """
+        This function will save tiles of the given H&E stained slide at different zoom levels.
+
+        Args:
+            - slide_loc: A .svs file of the H&E stained slides
+            - output_dir: The location where the tiles will be saved
+            - background: The maximum precentage of background allowed for a saved tile 
+            - size: The width and hight of the tiles at each zoom level
+
+        Returns:
+            - None
+    """
     slide = open_slide(slide_loc)
     dz = DeepZoomGenerator(slide, size, 0)
 
@@ -70,7 +95,7 @@ def tile(slide_loc, output_dir, background=0.2, size=255):
                 tile = dz.get_tile(level, (col, row))
                 tile_name = os.path.join(tile_dir, f"{col}_{row}")
 
-                avg_brightness = get_avg_brightness(tile)
+                avg_brightness = get_background_percentage(tile)
                 #tile = normalize_tile(tile)
 
                 if avg_brightness < background:
@@ -91,7 +116,7 @@ if __name__ == "__main__":
     parser = OptionParser(usage='Usage: %prog <slide> <output_folder> [options]')
     parser.add_option('-o', '--output', metavar='NAME', dest='output_dir', help='base name of output file')
     parser.add_option('-b', '--background', metavar='PIXELS', dest='background', type='float', default=0.2, help='Max background threshold [0.2]; percentage of background allowed')
-    parser.add_option('-s', '--size', metavar='PIXELS', dest='tile_size', type='int', default=255, help='tile size [255]')
+    parser.add_option('-s', '--size', metavar='PIXELS', dest='tile_size', type='int', default=255, help='tile size [255 ]')
     
     (opts, args) = parser.parse_args()
 
