d2920343ffbdbdb99150a90567533e535fbae316
Rory <2316980y@student.gla.ac.uk>
2020-11-12 13:14:43 +0000

    Update background detection

--- src/tile.py
+++ src/tile.py
@@ -11,8 +11,12 @@ from PIL import Image
 
 def get_avg_brightness(tile):
     grey = tile.convert("L")
-    bw = grey.point(lambda x: 0 if x< 200 else 1, "F")
-    return np.average(bw)
+    bw = grey.point(lambda x: 0 if x< 225 else 255, "F")
+
+    bw_arr = np.array(bw)
+    black_cells = np.count_nonzero(bw_arr == 0)
+    white_cells = np.count_nonzero(bw_arr == 255)
+    return white_cells/(white_cells+black_cells)
 
 
 def normalize_tile(tile):
@@ -42,7 +46,7 @@ def normalize_tile(tile):
     return Image.fromarray(normalized_tile)
 
 
-def tile(slide_loc, output_dir, background=0.1, size=255):
+def tile(slide_loc, output_dir, background=0.2, size=255):
     slide = open_slide(slide_loc)
     dz = DeepZoomGenerator(slide, size, 0)
 
@@ -67,16 +71,26 @@ def tile(slide_loc, output_dir, background=0.1, size=255):
                 tile_name = os.path.join(tile_dir, f"{col}_{row}")
 
                 avg_brightness = get_avg_brightness(tile)
+                #tile = normalize_tile(tile)
 
                 if avg_brightness < background:
-                    tile = normalize_tile(tile)
-                    tile.save(tile_name + ".jpeg")
+                    tile.save(f"{tile_name}.jpeg")
+
+                else:
+                    # if np.random.uniform() < 0.1:
+                    reject_dir = os.path.join(slide_output_dir, str(thisMag), "rejected")
+                    if not os.path.exists(reject_dir):
+                        os.makedirs(reject_dir)
+
+                    reject_tile_name = os.path.join(reject_dir, f"{col}_{row}_{avg_brightness}")
+                    tile.save(f"{reject_tile_name}.jpeg")
+                        
 
 
 if __name__ == "__main__":
     parser = OptionParser(usage='Usage: %prog <slide> <output_folder> [options]')
     parser.add_option('-o', '--output', metavar='NAME', dest='output_dir', help='base name of output file')
-    parser.add_option('-b', '--background', metavar='PIXELS', dest='background', type='float', default=0.1, help='Max background threshold [0.1]; percentage of background allowed')
+    parser.add_option('-b', '--background', metavar='PIXELS', dest='background', type='float', default=0.2, help='Max background threshold [0.1]; percentage of background allowed')
     parser.add_option('-s', '--size', metavar='PIXELS', dest='tile_size', type='int', default=255, help='tile size [299]')
     
     (opts, args) = parser.parse_args()
