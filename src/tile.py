e6ad9f1eae7dd24085532b5c041e6d1a3bbdbc0f
Rory <2316980y@student.gla.ac.uk>
2020-11-17 13:30:54 +0000

    Refactor tile saving at zoom levels

--- src/tile.py
+++ src/tile.py
@@ -8,6 +8,7 @@ from openslide.deepzoom import DeepZoomGenerator
 import staintools
 from PIL import Image
 import cv2
+import shutil
 
 
 def get_background_percentage(tile, threshold=225):
@@ -16,6 +17,7 @@ def get_background_percentage(tile, threshold=225):
 
         Args:
             - tile: A PIL Image object of the slide tile
+            - threshold: The maximum greyscale value for a pixel to be considered background
 
         Returns:
             - Float: The precentage of background in the tile
@@ -51,6 +53,46 @@ def normalize_tile(tile):
     return Image.fromarray(normalized_tile)
 
 
+def tile_level(dz, level, tile_dir, background, threshold):
+    """
+        This function will save all the relevant tiles for a given zoom level.
+
+        Args:
+            - dz: A DeepZoomGenerated object for the slide
+            - level: The level of zoom to save
+            - tile_dir: The output directory for this zoom level
+            - background: The maximum precentage of background allowed for a saved tile 
+            - threshold: The maximum greyscale value for a pixel to be considered background
+
+        Returns:
+            - None
+    """
+    if not os.path.exists(tile_dir):
+        os.makedirs(tile_dir)
+    
+    reject_dir = os.path.join(tile_dir, "rejected")
+    if not os.path.exists(reject_dir):
+        os.makedirs(reject_dir)
+
+    cols, rows = dz.level_tiles[level]
+    for row in range(rows):
+        for col in range(cols):
+            tile = dz.get_tile(level, (col, row))
+            tile_name = os.path.join(tile_dir, f"{col}_{row}")
+
+            tile_background = get_background_percentage(tile, threshold)
+            #tile = normalize_tile(tile)
+
+            if tile_background < background:
+                tile.save(f"{tile_name}.jpeg")
+
+            else:
+                if np.random.uniform() < 0.1:
+
+                    reject_tile_name = os.path.join(reject_dir, f"{col}_{row}_{tile_background}")
+                    tile.save(f"{reject_tile_name}.jpeg")
+
+
 def tile(slide_loc, output_dir, background=0.2, threshold=225, size=255):
     """
         This function will save tiles of the given H&E stained slide at different zoom levels.
@@ -59,6 +101,7 @@ def tile(slide_loc, output_dir, background=0.2, threshold=225, size=255):
             - slide_loc: A .svs file of the H&E stained slides
             - output_dir: The location where the tiles will be saved
             - background: The maximum precentage of background allowed for a saved tile 
+            - threshold: The maximum greyscale value for a pixel to be considered background
             - size: The width and hight of the tiles at each zoom level
 
         Returns:
@@ -67,40 +110,21 @@ def tile(slide_loc, output_dir, background=0.2, threshold=225, size=255):
     slide = open_slide(slide_loc)
     dz = DeepZoomGenerator(slide, size, 0)
 
-
     file_name = os.path.basename(slide_loc).split(".")[0]
     slide_output_dir = os.path.join(output_dir, file_name)
-    if not os.path.exists(slide_output_dir):
-        os.makedirs(slide_output_dir)
+
+    if os.path.exists(slide_output_dir):
+        shutil.rmtree(slide_output_dir)
+        
+    os.makedirs(slide_output_dir)
 
     maxZoom = float(slide.properties[openslide.PROPERTY_NAME_OBJECTIVE_POWER]) / slide.level_downsamples[0]
 
     for level in range(1, dz.level_count):
         thisMag = maxZoom/pow(2,dz.level_count-(level+1))
         tile_dir = os.path.join(slide_output_dir, str(thisMag))
-        if not os.path.exists(tile_dir):
-            os.makedirs(tile_dir)
 
-        cols, rows = dz.level_tiles[level]
-        for row in range(rows):
-            for col in range(cols):
-                tile = dz.get_tile(level, (col, row))
-                tile_name = os.path.join(tile_dir, f"{col}_{row}")
-
-                tile_background = get_background_percentage(tile, threshold)
-                #tile = normalize_tile(tile)
-
-                if tile_background < background:
-                    tile.save(f"{tile_name}.jpeg")
-
-                else:
-                    if np.random.uniform() < 0.1:
-                        reject_dir = os.path.join(slide_output_dir, str(thisMag), "rejected")
-                        if not os.path.exists(reject_dir):
-                            os.makedirs(reject_dir)
-
-                        reject_tile_name = os.path.join(reject_dir, f"{col}_{row}_{tile_background}")
-                        tile.save(f"{reject_tile_name}.jpeg")
+        tile_level(dz, level, tile_dir, background, threshold)
                         
 
 
